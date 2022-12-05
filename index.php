c690170eaa64a8fd5fb5f62c7b5cbae5b6ddc560
APOLLO SPENCER <119892064+Cash924@users.noreply.github.com>
2022-12-05 19:52:10 +0000

    3 extra lines

--- index.php
+++ index.php
@@ -3,7 +3,9 @@
     <title>PHP Test</title>
   </head>
   <body>
-    <?php echo '<p>Hello World</p>'; ?> 
+    <?php echo '<p>yo/p>'; ?> 
+		  <?php echo '<p>Hello World</p>'; ?> 
+		  <?php echo '<p>Hello World</p>'; ?> 
     <!--
     This script places a badge on your repl's full-browser view back to your repl's cover
     page. Try various colors for the theme: dark, light, red, orange, yellow, lime, green,
